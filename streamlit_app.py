54d354551be33fa9dc6622c7ed3f0a9b182b4fb3
Daniyal Khan <link2dak@gmail.com>
2026-09-16 16:46:09 -0500

    Update streamlit_app.py
    got rid of extra api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,9 +53,3 @@ if ingredients_list:
         session.sql(my_insert_smt).collect()
 
         st.success(f"Your Smoothie is ordered, {name_on_order}", icon="✅")
-
-
-# API for smoothiefroot
-smoothiefroot_response = requests.get(
-"https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
