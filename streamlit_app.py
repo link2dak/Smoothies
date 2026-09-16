ca5d8d0cc6f66bde00dd05b0400168cb6359ca0c
Daniyal Khan <link2dak@gmail.com>
2026-09-16 17:10:28 -0500

    Update streamlit_app.py
    added a pandas loc function

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,6 +38,10 @@ if ingredients_list:
     
     for i in ingredients_list:       
         ingredients_string+=i + ' '
+      
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+      
         # API for smoothiefroot for chosen fruit
         st.subheader(i+'Nutrition Information')
         smoothiefroot_response = requests.get(
